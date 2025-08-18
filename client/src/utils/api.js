73891e4fefc3f842cdf14b5381e6a841cc543b97
saad80201 <snadeeb02@gmail.com>
2025-08-18 11:33:50 +0600

    NEST V1.0 - modified API.JS

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = '/api';
+const API_BASE_URL = 'https://nest-home-rental-system.onrender.com/api';
 
 // Helper function to handle API responses
 const handleResponse = async (response) => {
